b398dee637c47cffcc5b5117b938ebe7f0a89f38
alex-mcf <socceralex360@gmail.com>
2025-05-09 12:42:20 -0600

    Organized file structure and created league config settings.

---
+++ FantasyOptimizer/data/config/league_config.py
@@ -0,0 +1,23 @@
+# Configure the settings of your league
+league_settings = {
+    "pts_per_pass_yard" : 0.04,
+    "pts_per_pass_td" : 4,
+    "pts_per_int_thrown" : -2,
+    "pts_per_rush_yard" : 0.1,
+    "pts_per_rush_td" : 6,
+    "pts_per_rec_yard" : 0.1,
+    "pts_per_rec_td" : 6,
+    "pts_per_reception" : 1,
+    "pts_per_fumble_lost" : -2,
+    "pts_per_2ptconv_success" : 2,
+    "positions": {
+        "QB" : 1, 
+        "RB" : 2,
+        "WR" : 2, 
+        "TE" : 1,
+        "FLEX" : 1, 
+        "K": 1,
+        "D/ST": 1,
+        "Bench": 6
+    }
+}
